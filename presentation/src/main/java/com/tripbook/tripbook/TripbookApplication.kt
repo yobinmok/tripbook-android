420edd02cd08f87574dd40561f24705e7a7bff30
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2023-05-26 01:03:46 +0900

    👷Android Logger 및 Dependabot 설정.

--- presentation/src/main/java/com/tripbook/tripbook/TripbookApplication.kt
+++ presentation/src/main/java/com/tripbook/tripbook/TripbookApplication.kt
@@ -1,7 +1,16 @@
 package com.tripbook.tripbook
 
 import android.app.Application
+import com.auth0.android.auth0.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class TripbookApplication : Application()
+class TripbookApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
