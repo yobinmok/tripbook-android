583f0402096ae270d39fb9e1311551991b999e4b
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-02-09 22:21:57 +0900

    Feature/junwoo/base url (#42)

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -44,6 +44,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+            isDebuggable = true
         }
     }
     compileOptions {
